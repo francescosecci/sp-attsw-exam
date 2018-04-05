67197707a945f504ea21b1dbce5c6c050cc5ed03
Alex Foglia <alex.f1793@gmail.com>
2018-04-05 10:48:28 +0200

    created multi modules project

---
+++ puccettifogliasecci/sp-server/src/main/java/org/sp/server/App.java
@@ -0,0 +1,13 @@
+package org.sp.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
